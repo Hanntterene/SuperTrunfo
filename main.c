f9f6babe3b8e0dc647dd9322dcabb3b800ffba2a
Nick <strapassonnicolas@gmail.com>
2024-12-05 12:41:10 -0300

    prototipo

--- main.c
+++ main.c
@@ -2,7 +2,73 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include "cartas.h"
+
+/**
+ * @brief 
+ * 
+ * @file main.c
+ * @author Nicolas, Lohanna, Fábio
+ * @date 2024-12-04
+*/
+
+
+/**
+ * @brief enum functionalities
+ * 
+ */
+typedef enum {INSERIR = 1, LISTAR, PESQUISAR, ALTERAR, EXCLUIR, SAIR} Funcionalidade;
+
+/**
+ * @brief struct Cartas
+ * 
+ */
+typedef struct {
+    char nome[40];
+    char letra;
+    char numero;
+    bool super_trunfo;
+    int influencia;
+    int a2;
+    int a3;
+    int a4;
+    bool carta_jogador;
+} Cartas;
+
+/* Estrutura para cartas específicas do jogador
+typedef struct {
+    char nome[40];
+    char letra;
+    char numero;
+    bool super_trunfo;
+    int influencia;
+    int a2;
+    int a3;
+    int a4;
+} Cartas_jogador;
+*/
+
+void exibeCartas (Cartas *lista, int i) {
+    if (lista[i].super_trunfo == 1) {
+        printf("SUPER TRUNFO\n");
+        printf("Carta %d:\n", i + 1);
+        printf("  Nome: %s\n", lista[i].nome);
+        printf("  Atributos: %d, %d, %d, %d\n", lista[i].influencia, lista[i].a2, lista[i].a3, lista[i].a4);
+        printf("  Código: %c-%d\n\n", lista[i].letra, lista[i].numero);
+    }
+    else {
+        printf("Carta %d:\n", i + 1);
+        printf("  Nome: %s\n", lista[i].nome);
+        printf("  Atributos: %d, %d, %d, %d\n", lista[i].influencia, lista[i].a2, lista[i].a3, lista[i].a4);
+        printf("  Código: %c-%d\n\n", lista[i].letra, lista[i].numero);
+    }
+}
+
+void preencheString (char pesquisa_nome[]) {
+    setbuf(stdin, NULL);
+    printf("Digite o nome da carta a ser pesquisada: ");
+    fgets(pesquisa_nome, 40, stdin);
+    pesquisa_nome[strcspn(pesquisa_nome, "\n")] = '\0';
+}
 
 int main() {
     int tamanho = 10;
@@ -10,7 +76,7 @@ int main() {
     int contador = 0;
     int coluna;
     char linhas[256];
-    Cartas lista[tamanho];
+    Cartas *lista = (Cartas*) malloc(sizeof(Cartas) * tamanho);
     //Cartas deck[tamanho / 2];
     FILE *file = fopen(filename, "r");
 
@@ -44,7 +110,7 @@ int main() {
                     lista[index].nome[sizeof(lista[index].nome) - 1] = '\0';
                     break;
                 case 3:
-                    lista[index].a1 = atoi(token); // Salvo como int (converte string -> int) 
+                    lista[index].influencia = atoi(token); // Salvo como int (converte string -> int) 
                     break;
                 case 4: 
                     lista[index].a2 = atoi(token);
@@ -71,10 +137,14 @@ int main() {
 
     fclose(file);
 
+    // variáveis correspondentes as pesquisas de usuário
     int escolha;
     int tipo_pesquisa;
     int modo;
     char pesquisa_nome[40];
+    // int pesquisa_valor_atributo;
+    bool existencia = 0;
+    char nova_pesquisa = 'n'; // condição inicial negada
 
 
     // MENU INCIAL (Edição, início do jogo ou finalização do programa)
@@ -110,22 +180,65 @@ int main() {
                     break;
                     case PESQUISAR:
                         do {
-                            printf("\n1 - Nome da carta\n");
-                            printf("2 - Atributo\n");
-                            printf("3 - Voltar\n");
+                            if (nova_pesquisa == 'n') { // Modo padrão pois nenhuma pesquisa foi realizada ou não foi solicitada
+                                printf("\n1 - Nome da carta\n");
+                                printf("2 - Atributo\n");
+                                printf("3 - Voltar\n");
 
-                            printf("Escolha o tipo de pesquisa: ");
-                            scanf("%d", &tipo_pesquisa);
+                                printf("Escolha o tipo de pesquisa: ");
+                                scanf("%d", &tipo_pesquisa);
+                            }
+                            else { // modo de pesquisa rápida
+                                tipo_pesquisa = 1;
+                            }
+
                             if (tipo_pesquisa == 1) {
-                                setbuf(stdin, NULL);
-                                printf("Digite o nome da carta a ser pesquisada: ");
-                                fgets(pesquisa_nome, 40, stdin);
-                                pesquisa_nome[strcspn(pesquisa_nome, "\n")] = '\0';
+                                preencheString(pesquisa_nome);
                                 for (int i = 0; i < tamanho; i += 1) {
                                     if (strcmp(pesquisa_nome, lista[i].nome) == 0) {
                                         exibeCartas(lista, i);
+                                        existencia = 1;
                                     }
                                 }
+                                if (existencia == 0) {
+                                    printf("Não foi possível encontrar nenhuma carta com o nome acima...\n");
+                                }
+                                do {
+                                    printf("Deseja pesquisar novamente? (s/n)\n");
+                                    printf("-> ");
+                                    setbuf(stdin, NULL);
+                                    scanf("%c", &nova_pesquisa);
+                                } while (nova_pesquisa != 's' || nova_pesquisa != 's');
+                                existencia = 0;
+                            }
+
+                            if (tipo_pesquisa == 2) {
+                                printf("   1 - Atributo\n");
+                                printf("   2 - ID (Letra ou Número)\n");
+                                printf("Escolha o tipo de pesquisa: \n");
+                                preencheString(pesquisa_nome);
+                                for (int i = 0; i < tamanho; i += 1) {
+                                    if(strcmp(pesquisa_nome, "influencia") == 0) {
+                                        printf("-> %s", lista[i].nome);
+                                        printf(" Influência: %d", lista[i].influencia);
+                                    }
+                                    else if(strcmp(pesquisa_nome, "influencia") == 0) {
+                                        printf("-> %s", lista[i].nome);
+                                        printf(" Influência: %d", lista[i].influencia);
+                                    }
+                                    else if(strcmp(pesquisa_nome, "influencia") == 0) {
+                                        printf("-> %s", lista[i].nome);
+                                        printf(" Influência: %d", lista[i].influencia);
+                                    }
+                                    else if(strcmp(pesquisa_nome, "influencia") == 0) {
+                                        printf("-> %s", lista[i].nome);
+                                        printf(" Influência: %d", lista[i].influencia);
+                                    }
+                                    else {
+                                        printf("Não foi encontrado nenhum atributo referente a pesquisa\n");
+                                    }
+                                }
+
                             }
                         } while (tipo_pesquisa != 3);
                     break;
@@ -144,7 +257,17 @@ int main() {
                     break;
                 }
             } while (escolha != 6);
-        }   
+        } 
+
+        else if (modo == 2) {
+            printf("Desculpe o modo de batalha ainda não está disponível.\n");
+        }
+
+        else if (modo > 3 || modo < 1) {
+            printf("Desulpe, essa opção não está disponível...\n");
+        }
     } while (modo != 3);
+
+    printf("Obrigado por jogar! Seus dados ficarão salvos para nosso próximo encontro!\n");
     return 0;
 }
